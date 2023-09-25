b841d126af9a7cd995072f58a0356faec92af859
AleksandrRogov25 <alrogov25@gmail.com>
2023-09-25 23:44:58 +0300

    HomeWork1

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+// Вариант 1
+// if (a > b && a > c)
+// {
+// Console.WriteLine($"maх = {a} ");
+// }
+// else
+// {
+//      if (b > c){
+// Console.WriteLine($"maх = {b} ");
+// }
+//     if (c > b){
+// Console.WriteLine($"maх = {c} ");
+// }
+// }
+//Вариант 2
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine($"maх = {max} ");
---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+int number1 = 1;
+while (number1 <= number)
+{
+    if (number1 % 2 == 0)
+Console.Write(number1 + ", "); 
+number1++;
+}
+}
+else
+{
+Console.WriteLine("Введите положительное число!");
+}
+
